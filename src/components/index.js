96c51c18d6a05621815ee7be4fe74f43463cb59b
nazar2419 <nazarx095@gmail.com>
2020-10-18 18:17:25 +0300

    add-router

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './Button'; //import and export в одну строчку
 export { default as Header } from './Header';
 export { default as Categories } from './Categories';
+export { default as SortPopup } from './SortPopup';
 
